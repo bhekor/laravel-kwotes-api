5842d60c64b1559b2b14f118be57a3033a14e0d1
Ibidapo Adeolu <adeoluibidapo@gmail.com>
2021-01-06 19:20:59 +0100

    Some Cleaning

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -2,20 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Quote;
 use Illuminate\Http\Request;
 
 class PagesController extends Controller
 {
     public function index()
     {
-        return view('pages.index');
-    }
-    public function all()
-    {
-        $quotes = Quote::all();
-        return view('pages.all', [
-            'quotes' => $quotes,
-        ]);
+        return view('welcome');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,6 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', [PagesController::class, 'index'])->name('index');
-Route::get('/all', [PagesController::class, 'all'])->name('all');
 
 
 
